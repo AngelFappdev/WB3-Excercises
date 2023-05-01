36d5edd8a2204d470836abf084cc5f1b6d91032e
Angel Fraser <Angelaffirms@gmail.com>
2023-05-01 11:26:31 -0400

    displayed fav things

---
+++ Functions/simple_functions.js
@@ -0,0 +1,19 @@
+function displayfavthings()
+    {
+        console.log("Angel Fraser")
+        console.log("In the Mood For Love")
+        console.log("LOVE U JESSIE WARE")
+    }
+displayfavthings();
+
+function displaywhyimhere()
+    {
+        console.log("I joined this program to learn marketable skills to allow myself to have a stable career.")
+    }
+displaywhyimhere();
+
+function displayfavplace()
+    {
+        console.log("Frenchmans cove in Jamaica. My family is jamaican so it felt like home and the water was crisp and clear.")
+    }
+displayfavplace();
