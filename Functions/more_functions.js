6139cbb4bf186ca340ee1f25094da73dbe744035
Angel Fraser <Angelaffirms@gmail.com>
2023-05-01 13:47:13 -0400

    part 3 of excerise 2

--- Functions/more_functions.js
+++ Functions/more_functions.js
@@ -1,11 +1,25 @@
-function displayMailinglabel(name, address, city, state, zip)
-    {
-        console.log(`
-        ${name}
-        ${address}
-        ${ city }, ${state} ${zip}
+ function displayMailinglabel(name, address, city, state, zip)
+     {
+         console.log(`
+         ${name}
+         ${address}
+         ${ city }, ${state} ${zip}
         `);
     }
-displayMailinglabel("Angel", "400 Cherry Street", "Freeport", "New York", "11520");
+ displayMailinglabel("Angel", "400 Cherry Street", "Freeport", "New York", "11520")
+function displayaddNumbers(num1, num2){
+     let somenumber = (num1 + num2);
+      console.log(somenumber)
 
-    
+ }
+ displayaddNumbers(200,400);
+
+
+function displayReceipt(totaldue , amountpaid){
+    console.log("Total Due:", totaldue ,"Amount Paid:", amountpaid)
+    if(
+        amountpaid < totaldue)
+        console.log("Pay the difference")
+
+}
+displayReceipt(500,500)
