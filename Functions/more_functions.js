1de1032d54d4bab54b931bab84a954a152cb7bd3
Angel Fraser <Angelaffirms@gmail.com>
2023-05-01 13:14:45 -0400

    kevin's demo

---
+++ Functions/more_functions.js
@@ -0,0 +1,11 @@
+function displayMailinglabel(name, address, city, state, zip)
+    {
+        console.log(`
+        ${name}
+        ${address}
+        ${ city }, ${state} ${zip}
+        `);
+    }
+displayMailinglabel("Angel", "400 Cherry Street", "Freeport", "New York", "11520");
+
+    
